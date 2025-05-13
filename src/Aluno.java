0ffcf31ffbde3517bcd31b7d0373cb0c063e1df7
RicardoLMessias <ricardo.lima.messias@gmail.com>
2025-05-12 21:58:31 -0300

    novo commit

---
+++ src/Aluno.java
@@ -0,0 +1,30 @@
+import java.util.Objects;
+
+public class Aluno {
+    private String nome;
+
+    public Aluno(String aluno) {
+        this.nome = aluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+
+        Aluno outro = (Aluno) obj;
+        return outro.getNome().equals(this.nome);
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
